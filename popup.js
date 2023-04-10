b0dfe9fb0db279bcc66cdfb5ee98d988622f2e8f
itsfuad <fuad.cs22@gmail.com>
2023-04-11 00:44:30 +0600

    ,..

--- popup.js
+++ popup.js
@@ -1,12 +1,32 @@
 // Generate a random password with uppercase letters, lowercase letters, and digits
 function generatePassword(length) {
-    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let chars = '';
+
+    console.log(!includeNumbers.checked && !includeSymbols.checked && !includeUppercase.checked && !includeLowercase.checked);
+
+    if (!includeNumbers.checked && !includeSymbols.checked && !includeUppercase.checked && !includeLowercase.checked) {
+        return [false, 'Choose option'];
+    }
+
+    if (includeNumbers.checked) {
+        chars += '0123456789';
+    }
+    if (includeSymbols.checked) {
+        chars += '!@#$%^&*_+~?><=';
+    }
+    if (includeUppercase.checked) {
+        chars += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    }
+    if (includeLowercase.checked) {
+        chars += 'abcdefghijklmnopqrstuvwxyz';
+    }
+
     let password = '';
     for (let i = 0; i < length; i++) {
         const index = Math.floor(Math.random() * chars.length);
         password += chars.charAt(index);
     }
-    return password;
+    return [true, password];
 }
 
 let infoTimeout = undefined;
@@ -15,8 +35,15 @@ let infoTimeout = undefined;
 function generateNewPassword() {
     const show = document.getElementById('show');
     const length = passLen.value;
-    const password = generatePassword(length); // Change the length here if desired
+
+    const [success, password] = generatePassword(length);
+    
     show.textContent = password;
+
+    if (!success) {
+        return;
+    }
+
     //copy to clipboard
     navigator.clipboard.writeText(password);
 
@@ -30,14 +57,14 @@ function generateNewPassword() {
 
     
 
-    generateButton.style.backgroundColor = 'lime';
+    //generateButton.style.backgroundColor = 'lime';
     generateButton.textContent = 'Copied!';
     //clear info after 3 seconds
     if (infoTimeout) {
         clearTimeout(infoTimeout);
     }
     infoTimeout = setTimeout(() => {
-        generateButton.style.backgroundColor = 'var(--button-color)';
+        //generateButton.style.backgroundColor = 'var(--button-color)';
         generateButton.textContent = 'Generate';
         show.textContent = '----------';
     }, 2000);
@@ -48,20 +75,91 @@ const generateButton = document.getElementById('generateButton');
 const lenLabel = document.getElementById('lenLabel');
 const passLen = document.getElementById('passLen');
 const recentBtn = document.getElementById('recentBtn');
+const includeNumbers = document.getElementById('includeNumbers');
+const includeSymbols = document.getElementById('includeSymbols');
+const includeUppercase = document.getElementById('includeUppercase');
+const includeLowercase = document.getElementById('includeLowercase');
+
+//find if localstorage has include checkbox values
+const includeNumbersValue = localStorage.getItem('includeNumbers');
+const includeSymbolsValue = localStorage.getItem('includeSymbols');
+const includeUppercaseValue = localStorage.getItem('includeUppercase');
+const includeLowercaseValue = localStorage.getItem('includeLowercase');
+
+if (!includeNumbersValue) {
+    localStorage.setItem('includeNumbers', true);
+    includeNumbers.checked = true;
+} else {
+    includeNumbers.checked = includeNumbersValue === 'true';
+}
+
+if (!includeSymbolsValue) {
+    localStorage.setItem('includeSymbols', true);
+    includeSymbols.checked = true;
+} else {
+    includeSymbols.checked = includeSymbolsValue === 'true';
+}
+
+if (!includeUppercaseValue) {
+    localStorage.setItem('includeUppercase', true);
+    includeUppercase.checked = true;
+
+} else {
+    includeUppercase.checked = includeUppercaseValue === 'true';
+}
+
+if (!includeLowercaseValue) {
+    localStorage.setItem('includeLowercase', true);
+    includeLowercase.checked = true;
+} else {
+    includeLowercase.checked = includeLowercaseValue === 'true';
+}
+
+//find if localstorage has passLen value
+const passLenValue = localStorage.getItem('passLen');
+if (!passLenValue) {
+    localStorage.setItem('passLen', 10);
+    passLen.value = 10;
+    lenLabel.textContent = `Password length: ${passLen.value}`;
+} else {
+    passLen.value = passLenValue;
+    lenLabel.textContent = `Password length: ${passLen.value}`;
+}
+
+includeNumbers.addEventListener('change', () => {
+    localStorage.setItem('includeNumbers', includeNumbers.checked);
+});
+
+includeSymbols.addEventListener('change', () => {
+    localStorage.setItem('includeSymbols', includeSymbols.checked);
+});
+
+includeUppercase.addEventListener('change', () => {
+    localStorage.setItem('includeUppercase', includeUppercase.checked);
+});
+
+includeLowercase.addEventListener('change', () => {
+    localStorage.setItem('includeLowercase', includeLowercase.checked);
+});
+
 generateButton.addEventListener('click', generateNewPassword);
 
 passLen.addEventListener('input', () => {
     lenLabel.textContent = `Password length: ${passLen.value}`;
+    localStorage.setItem('passLen', passLen.value);
 });
 
 recentBtn.addEventListener('click', () => {
     //load recent passwords
     const recentPasswords = JSON.parse(localStorage.getItem('recentPasswords')) || [];
     
+
+
     const recentPasswordsContainer = document.getElementById('recentPasswordsContainer');
     if (recentPasswords.length === 0) {
         recentPasswordsContainer.innerHTML = '<p class="empty">No recent passwords</p>';
     }
+    recentPasswordsContainer.innerHTML = '';
     recentPasswords.forEach((password) => {
         const recentPassword = document.createElement('div');
         recentPassword.classList.add('recentPassword');
---
+++ readme.md
@@ -0,0 +1,42 @@
+# Password Generator Chrome Extension
+
+This is a Chrome extension that generates passwords of varying lengths and displays the last 10 generated passwords.
+
+![Password Generator Interface Screenshot](ui1.png)
+![Password Generator Interface Screenshot](ui2.png)
+
+## Features
+
+- Generates passwords from 8-20 characters
+- Displays the last 10 generated passwords
+- Easy-to-use interface
+- Secure and reliable password generation
+
+## Installation
+
+To install the Password Generator Chrome Extension, follow these steps:
+
+1. Download the extension from the Chrome Web Store or GitHub
+2. Unzip the downloaded file (if necessary)
+3. Open Chrome and navigate to the Extensions settings (chrome://extensions/)
+4. Enable Developer Mode in the top right corner
+5. Click on the "Load unpacked" button in the top left corner
+6. Navigate to the unzipped folder and select it
+
+The Password Generator Chrome Extension should now be installed and ready to use.
+
+## Usage
+
+To use the Password Generator Chrome Extension, click on the extension icon in the toolbar. This will open the password generator interface.
+
+To generate a password, select the desired length using the dropdown menu and click the "Generate" button. The generated password will appear in the text box. The password will be copied automatically in your clip.
+
+The last 10 generated passwords are displayed in the "Htstory" button on the top right corner.
+
+## Contributing
+
+If you find any bugs or have suggestions for improving the Password Generator Chrome Extension, please open an issue on GitHub or submit a pull request.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
